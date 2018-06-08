176ccbccd0bab8c49eec9d66bec442481c61a899
KadyrovEldos <eldos.kadyrov@gmail.com>
2018-06-08 19:52:12 +0600

    Webpack settings

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,12 +2,14 @@
 <html lang="{{ app()->getLocale() }}" class="js">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <title>
         @yield('title')
     </title>
 
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico')}}">
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 
@@ -21,7 +23,6 @@
     
     <script src="{{ mix('/js/app.js')}}"></script>
     @yield('scripts')
-
 
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,13 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.scripts([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+    'resources/assets/js/app.js'
+],  'public/js/app.js')
+    .version();
+
+
+mix.sass('resources/assets/scss/app.scss', 'public/css')
+    .version();
