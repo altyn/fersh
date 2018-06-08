49d6c45e6fc9bb0d733d6c33244e2c4749bf5356
Altynbek Shaidyldaev <altynbek.shaidyldaev@gmail.com>
2018-06-08 16:46:38 +0600

    Auth Providers added

---
+++ app/Models/User/ModelName.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models\User;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
+class ModelName extends Model implements AuthenticatableContract, CanResetPasswordContract
+{
+    use Authenticatable, CanResetPassword;
+
+    protected $table = 'users';
+
+    protected $hidden = ['password', 'remember_token'];
+
+    protected $guarded = ['id'];
+
+    protected $fillable = [
+        'login',
+        'email',
+        'freelancer',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function isFreelancer()
+    {
+        return $this->freelancer;
+    }
+
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+}
