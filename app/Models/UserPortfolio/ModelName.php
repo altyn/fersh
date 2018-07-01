2086444f6b2d365c7db0f96b37a60ea2378b15c2
KadyrovEldos <eldos.kadyrov@gmail.com>
2018-07-01 22:31:50 +0600

    Upload portfolio

---
+++ app/Models/UserPortfolio/ModelName.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models\UserPortfolio;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User\ModelName as User;
+
+class ModelName extends Model
+{
+    protected $table = 'portfolio';
+
+    protected $guarded = ['id'];
+
+    protected $fillable = [
+        'description',
+        'files',
+        'links',
+        'tags',
+        'views',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $casts = [
+        'description' => 'json',
+        'files' => 'json',
+        'links' => 'json',
+        'tags' => 'json',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Methods
+    public function getDate()
+    {
+        $fullDate = $this->created_at;
+        $date = date('d.m.Y', strtotime($fullDate));
+        return $date;
+    }
+
+}
