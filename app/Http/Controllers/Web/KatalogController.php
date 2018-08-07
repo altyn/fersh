aa0b78d08717acbd998975708b91c13c37d511c7
KadyrovEldos <eldos.kadyrov@gmail.com>
2018-08-08 02:40:03 +0600

    Search Result change

--- app/Http/Controllers/Web/KatalogController.php
+++ app/Http/Controllers/Web/KatalogController.php
@@ -30,21 +30,14 @@ class KatalogController extends Controller
                         ->whereNotNull('avatar');
                 })
             ->get();
-//            $freelancers = UserDetails::where(function ($query) use($keyword) {
-////                    $query->whereRaw(DB::raw('upper(spec->"$.ru.skills")'), 'LIKE', '%'.$keyword.'%')->where('freelancer', 1)->whereNotNull('avatar');
-//                    $query->whereRaw('LOWER(spec->ru->skills) LIKE ? ','%'.[trim(strtolower($keyword)).'%'])->where('freelancer', 1)->whereNotNull('avatar');
-////                SELECT * FROM user_details WHERE UPPER(spec->"$.ru.skills") LIKE UPPER('%anGuLAR%');
-//                })
-//            ->get();
-            // $freelancers = UserDetails::where(function ($query) use($keyword) {
-            //         $query->where('first_name', 'like', '%' . $keyword . '%')
-            //         ->orWhere('last_name', 'like', '%' . $keyword . '%')->where('freelancer', 1)->whereNotNull('avatar');
-            //     })
-            // ->get();
-
             if($freelancers){
                 
                 foreach ($freelancers as $user) {
+                    if(isset($user->getSphere()->title)){
+                        $spheretext = $user->getsphere()->title['ru'];
+                    }else{
+                        $spheretext = '';
+                    }
                     $output.='<div class="col-lg-4 col-md-6 col-sm-12">'.
                         '<div class="user-item">'.
                             '<div class="user-item-picture">'.
@@ -54,6 +47,7 @@ class KatalogController extends Controller
                             '</div>'.
                             '<div class="user-item-info">'.
                                 '<div class="user-item-info-name"><a href="/'.app()->getLocale().'/freelancer/'.$user->user_id.'">'.$user->getFio().'</a></div>'.
+                                '<div class="user-item-info-spec"><a href="/'.app()->getLocale().'/freelancer/'.$user->user_id.'">'.$spheretext.'</a></div>'.
                                 '<div class="user-item-info-desc"><article>'.$user->getShortBio().'</article></div>'.
                             '</div>'.
                         '</div>'.
